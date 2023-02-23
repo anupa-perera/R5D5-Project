7c254dbc173faac1928b14473a1221f019de46e0
Dulara-01 <124384611+Dulara-01@users.noreply.github.com>
2023-02-23 22:37:29 +0530

    update home pg 94% completed

--- react/frontend-look/src/_codux/boards/home-pg.board.tsx
+++ react/frontend-look/src/_codux/boards/home-pg.board.tsx
@@ -22,12 +22,42 @@ export default createBoard({
                 className="image3"
             />
             <br />
-            <Footer />
+            <h2 className="pop-players">
+                Most Populer Players
+                <br />
+            </h2>
+            <img
+                src="https://upload.wikimedia.org/wikipedia/commons/b/bf/LeBron_James_-_51959723161_%28cropped%29.jpg"
+                className="home-players"
+            />
+            <img
+                src="https://cdn.britannica.com/88/181288-050-212FB60C/Kevin-Durant-NBA.jpg"
+                className="home-players"
+            />
+            <img
+                src="https://staticc.sportskeeda.com/editor/2023/02/516ea-16764363358993-1920.jpg"
+                className="home-players"
+            />
+            <br />
+            <img
+                src="https://www.sportsnet.ca/wp-content/uploads/2022/04/lakers-westbrook-1040x572.jpg"
+                className="home-players"
+            />
+            <img
+                src="https://cdn.britannica.com/88/163888-050-D9D34015/Dwyane-Wade-NBA-Miami-Heat.jpg"
+                className="home-players"
+            />
+            <img
+                src="https://ca-times.brightspotcdn.com/dims4/default/b6c85ed/2147483647/strip/false/crop/3840x2160+0+0/resize/1486x836!/quality/80/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2F72%2Ff8%2F1aa9f1be432086e01741fdd93230%2Fnets-basketball-11370.jpg"
+                className="home-players"
+            />
+            <Footer className="footer" />
         </div>
     ),
     environmentProps: {
         canvasWidth: 1224,
-        windowHeight: 613,
+        windowHeight: 573,
         windowWidth: 1370,
+        canvasHeight: 1486,
     },
 });
