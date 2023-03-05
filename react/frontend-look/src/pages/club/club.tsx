4f8e03bd5c446d1b3649ca5263af0549c9614b51
Sasiru_Vishmika2001 <Sasiruvishmika@gmail.com>
2023-03-05 16:55:24 +0530

    Backend Server and DB

--- react/frontend-look/src/pages/club/club.tsx
+++ react/frontend-look/src/pages/club/club.tsx
@@ -1,11 +1,74 @@
 import React from 'react';
+import Classnames from 'classnames';
+import styles from './club.scss';
 
 export interface ClubProps {
     className?: string;
 }
 
-export const Club: React.FC<ClubProps> = ({ className = '' }) => (
-    <div className={className}>Club
-    <br/> This page will discribe the club and then show all the players in the club using tiles
-    </div>
-);
+export const Club: React.FC<ClubProps> = ({ className = '' }) => {
+    return (
+            <div className="container">
+                <div className="tile">
+                    <h3>Name One</h3>
+                </div>
+                <div className="tile">
+                    <h3>Tile 2</h3>
+                </div>
+                <div className="tile">
+                    <h3>Tile 3</h3>
+                </div>
+                <div className="tile">
+                    <h3>Tile 4</h3>
+                </div>
+                <div className="tile">
+                    <h3>Tile 5</h3>
+                </div>
+                <div className="tile">
+                    <h3>Tile 6</h3>
+                </div>
+                <div className="tile">
+                    <h3>Tile 7</h3>
+                </div>
+                <div className="tile">
+                    <h3>Tile 8</h3>
+                </div>
+                <div className="tile">
+                    <h3>Tile 9</h3>
+                </div>
+                <div className="tile">
+                    <h3>Tile 10</h3>
+                </div>
+                <div className="tile">
+                    <h3>Tile 11</h3>
+                </div>
+                <div className="tile">
+                    <h3>Tile 12</h3>
+                </div>
+                <div className="tile">
+                    <h3>Tile 13</h3>
+                </div>
+                <div className="tile">
+                    <h3>Tile 14</h3>
+                </div>
+                <div className="tile">
+                    <h3>Tile 15</h3>
+                </div>
+                <div className="tile">
+                    <h3>Tile 16</h3>
+                </div>
+                <div className="tile">
+                    <h3>Tile 17</h3>
+                </div>
+                <div className="tile">
+                    <h3>Tile 18</h3>
+                </div>
+                <div className="tile">
+                    <h3>Tile 19</h3>
+                </div>
+                <div className="tile">
+                    <h3>Tile 20</h3>
+                </div>
+            </div>
+    );
+};
