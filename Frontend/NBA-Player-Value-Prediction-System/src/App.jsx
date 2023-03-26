955973f744e3121ceeda5c0627d1ce82581117ab
malithalex <101708349+malithalex@users.noreply.github.com>
2023-03-26 10:00:11 +0530

    Revert "clubpage test working"
    This reverts commit 4a00542cf003d178ddb40ea2bef91922b286f273.

--- Frontend/NBA-Player-Value-Prediction-System/src/App.jsx
+++ Frontend/NBA-Player-Value-Prediction-System/src/App.jsx
@@ -11,7 +11,6 @@ import Clubs from './pages/clubs/clubs';
 import MyApp from './pages/dummypage/dummypage';
 import Clubplayers from './pages/players/clubplayers';
 
-
 function App() {
   const [count, setCount] = useState(0)
   const [club, setClub] = useState('')
--- Frontend/NBA-Player-Value-Prediction-System/src/pages/clubs/clubs.jsx
+++ Frontend/NBA-Player-Value-Prediction-System/src/pages/clubs/clubs.jsx
@@ -3,28 +3,16 @@ import { Link } from 'react-router-dom';
 import './clubs.css'
 import axios from 'axios';
 import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import pageheader from '../../assets/images/clubpage.jpg'
-import teams from './teams.json';
-import Card from './Cards';
 
 export default function Club({setClub}){
 
-    const cards = teams.map(item => {
-        return (
-            <Card 
-                key={item.ID}  
-                TeamName={item.TeamName}
-                arena={item.ArenaName}
-                location={item.ArenaLocation}
-                seating={item.SeatingCapacity}
-                year={item.OpeningYear}
-            />
-        )
-    })        
+    const [teams, setTeams] = useState([]);
 
     useEffect(() => {
         axios.get('http://localhost:8080/')
@@ -45,14 +33,24 @@ export default function Club({setClub}){
         </div>
         <div className='clubs'>
         <h3>Explore NBA Clubs</h3>
-        <Box sx={{display:'flex',flexWrap:'wrap',justifyContent:'center',gap:'0.5rem'}}>
-        {cards}
 
+          <Box sx={{display:'flex',flexWrap:'wrap',justifyContent:'center',gap:'0.5rem'}}>
+           {teams.map((team) => {
+              return(
+                <Card sx={{ width:'20rem'}}>
+                    <CardContent>
+                      <Typography variant="h5" component="div">
+                        {team.TeamName}
+                      </Typography>
+                    </CardContent>
+                    <CardActions>
                       <Link to="/clubpage/clubplayers" onClick={()=>{setClub(team.TeamName)}}>
                         <Button size="small">Players</Button>
                       </Link>
-                    
-        
+                    </CardActions>
+                </Card>
+                );
+           })}
           </Box> 
         </div>
       </React.Fragment>
