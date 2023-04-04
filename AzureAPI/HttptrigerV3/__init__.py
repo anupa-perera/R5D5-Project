fd6c284048587544aaf514092f7b124ec1d3724b
acez0z <anupaxtream@gmail.com>
2023-04-04 21:53:21 +0530

    API commit

--- AzureAPI/HttptrigerV3/__init__.py
+++ AzureAPI/HttptrigerV3/__init__.py
@@ -2,38 +2,56 @@ import logging
 import joblib
 import pandas as pd
 import azure.functions as func
-import os
 
 # Import the ML model
-model = joblib.load("HttptrigerV3\modelNBA.joblib")
+model = joblib.load("./HttptrigerV3/model.joblib")
 
-def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Received POST Requet.')
+def predict(req_body):
 
-    try:
-        req_body = req.get_json()
+    # Convert input data to DataFrame
+    logging.info('Converting the data into DataFrame.')
+    df = pd.DataFrame(req_body, index=[0])
 
-        # Convert input data to DataFrame
-        df = pd.DataFrame(req_body, index=[0])
+    # Make predictions
+    logging.info('Making predictions.')
+    predictions = model.predict(df)
 
-        # Make predictions
-        predictions = model.predict(df)
+    # Denormalize the predictions
+    logging.info('Denormalize the predictions.')
+    salary_min = 50000
+    salary_max = 45780966
+    predictions_denorm = int(predictions[0] * (salary_max - salary_min) + salary_min)
 
-        # Denormalize the predictions
-        salary_min = 50000
-        salary_max = 45780966
-        predictions_denorm = int(predictions[0] * (salary_max - salary_min) + salary_min)
+    # Format the output with comma separators
+    logging.info('Preparing the result.')
+    predictions_formatted = "{:,.0f}".format(predictions_denorm)
+    
+    return predictions_formatted
 
-        # Format the output with comma separators
-        predictions_formatted = "{:,.0f}".format(predictions_denorm)
 
-        # Return the predictions as a JSON object
-        return func.HttpResponse(f"{predictions_formatted}", status_code=200)
-    except ValueError:
-        pass
-
+def main(request: func.HttpRequest
+    ) -> func.HttpResponse:
+    
+    if request.method == "POST":
+        try:
+            logging.info('Started processing the request.')
+            data = request.get_json()
+            
+            if data == '':
+                raise ValueError
+            else:
+                logging.info('Process completed successfully.')
+                prediction = predict(data)
+                
+                return func.HttpResponse(f"{prediction}", status_code=200)
+                   
+        except ValueError:
+            logging.info('Got an empty body. The process has been terminated.')
+            return func.HttpResponse("Empty body, please make sure that your request is valid!", status_code=404)
+        
+        except Exception:
+            logging.info('Got an invalid request. The process has been terminated.')
+            return func.HttpResponse("Invalid request, please make sure that your request is valid!", status_code=400) 
     else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+        logging.info('Received a GET request.')
+        return func.HttpResponse("The server is up and running.", status_code=200)
